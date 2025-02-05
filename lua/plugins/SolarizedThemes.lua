9836adf443be4634f233fffc3819ed8ab9c34c7a
GasparVardanyan <gaspar_pm@proton.me>
2025-02-05 09:19:42 +0400

    backup

--- lua/plugins/SolarizedThemes.lua
+++ lua/plugins/SolarizedThemes.lua
@@ -28,5 +28,15 @@ return {
 			terminal_colors = false,
 		},
 		config = true,
-	}
+	},
 }
+
+-- https://github.com/altercation/vim-colors-solarized
+-- https://github.com/lifepillar/vim-solarized8
+-- https://github.com/overcache/NeoSolarized
+-- https://github.com/romainl/flattened
+-- https://github.com/jan-warchol/selenized
+-- https://github.com/svrana/neosolarized.nvim
+-- https://github.com/ishan9299/nvim-solarized-lua
+-- https://github.com/maxmx03/solarized.nvim
+-- https://github.com/calind/selenized.nvim
