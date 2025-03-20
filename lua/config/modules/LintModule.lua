f5d472e7c749041b317cf18279a6441bee728462
GasparVardanyan <gaspar_pm@proton.me>
2025-03-20 08:57:45 +0400

    backup

---
+++ lua/config/modules/LintModule.lua
@@ -0,0 +1,9 @@
+local Module = require ("config.Module")
+
+local LintModule = Module:new ({
+	name = "Lint",
+	plugins = {
+	},
+})
+
+return LintModule
---
+++ lua/config/modules/TestModule.lua
@@ -0,0 +1,9 @@
+local Module = require ("config.Module")
+
+local TestModule = Module:new ({
+	name = "Test",
+	plugins = {
+	},
+})
+
+return TestModule
