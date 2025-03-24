ed620061b26aa5e6bae59446cad0b79aec94092c
GasparVardanyan <gaspar_pm@proton.me>
2025-03-24 15:06:06 +0400

    backup

--- lua/modular/modules/DapModule.lua
+++ lua/modular/modules/DapModule.lua
@@ -8,12 +8,19 @@ local DapModule = Module:new ({
 		require ("modular.specs.Dap.nvim-dap"),
 		require ("modular.specs.Dap.mason-nvim-dap"),
 		require ("modular.specs.Dap.nvim-dap-ui"),
+		require ("modular.specs.Dap.telescope-dap_nvim"),
 	},
 	post_plugin_load_actions = {
 		PostPluginLoadAction:new ({
 			plugins = "nvim-dap",
 			action = function ()
 				require ("modular.mappings.Dap.nvim-dap")
+			end
+		}),
+		PostPluginLoadAction:new ({
+			plugins = { "telescope-dap.nvim", "telescope.nvim" },
+			action = function ()
+				require ("modular.mappings.Dap.telescope-dap_telescope_nvim")
 			end
 		})
 	}
