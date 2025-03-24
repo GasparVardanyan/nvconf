608a8c078fe080d289ea47d57df31978a42d5f5c
GasparVardanyan <gaspar_pm@proton.me>
2025-03-24 16:33:38 +0400

    backup

--- lua/modular/mappings/Dap/telescope-dap_telescope_nvim.lua
+++ lua/modular/mappings/Dap/telescope-dap_telescope_nvim.lua
@@ -1,4 +1,7 @@
 local map = vim.keymap.set
 
--- TODO: DAP TELESCOPE MAPPINGS
-print ("TODO: DAP TELESCOPE MAPPINGS")
+map ("n", "<leader>dfc", require ("telescope").extensions.dap.commands)
+map ("n", "<leader>dfC", require ("telescope").extensions.dap.configurations)
+map ("n", "<leader>dfb", require ("telescope").extensions.dap.list_breakpoints)
+map ("n", "<leader>dfv", require ("telescope").extensions.dap.variables)
+map ("n", "<leader>dff", require ("telescope").extensions.dap.frames)
