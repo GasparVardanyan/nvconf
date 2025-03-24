58c6743d598bf6c3a1750b550c9b0a668cf8f226
GasparVardanyan <gaspar_pm@proton.me>
2025-03-24 08:07:21 +0400

    backup

--- lua/modular/mappings/Dap/nvim-dap.lua
+++ lua/modular/mappings/Dap/nvim-dap.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 
 map('n', '<leader>db', vim.cmd.DapToggleBreakpoint)
+map('n', '<leader>dB', function()
+	require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+end)
 map('n', '<leader>dc', vim.cmd.DapContinue)
 map('n', '<leader>dd', vim.cmd.DapDisconnect)
 map('n', '<leader>dp', vim.cmd.DapPause)
