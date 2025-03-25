075868ee3362e22323a6fcfe4aa93a80aed28bbc
GasparVardanyan <gaspar_pm@proton.me>
2025-03-25 09:20:55 +0400

    backup

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
 require ("modular")
-
--- RULES:
--- no module calls another one
--- modules are used only from modes
--- modes can import multiple modules
--- modes can be combined
-
-
--- continue from here:
---   https://youtu.be/S-xzYgTLVJE?list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&t=703
---
+++ lua/modular/extras/modules/MenuModule.lua
@@ -0,0 +1,36 @@
+local Module = require ("modular.Module")
+local PostPluginLoadAction = require ("modular.PostPluginLoadAction")
+
+local MenuModule = Module:new ({
+	name = "Menu",
+	plugins = {
+		require ("modular.extras.specs.Menu.menu"),
+		require ("modular.extras.specs.Menu.volt"),
+	},
+	post_plugin_load_actions = {
+		PostPluginLoadAction:new ({
+			plugins = "menu",
+			action = function ()
+				-- Keyboard users
+				vim.keymap.set("n", "<C-t>", function()
+					require("menu").open("default")
+				end, {})
+
+				-- mouse users + nvimtree users!
+				vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+					require('menu.utils').delete_old_menus()
+
+					vim.cmd.exec '"normal! \\<RightMouse>"'
+
+					-- clicked buf
+					local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+					local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+					require("menu").open(options, { mouse = true })
+				end, {})
+			end
+		})
+	}
+})
+
+return MenuModule
