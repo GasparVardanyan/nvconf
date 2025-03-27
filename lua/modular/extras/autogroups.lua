779baa1c19012bd60b0199e035095d2dcde86ad7
GasparVardanyan <gaspar_pm@proton.me>
2025-03-27 21:03:52 +0400

    new lsp mappings

--- lua/modular/extras/autogroups.lua
+++ lua/modular/extras/autogroups.lua
@@ -1,6 +1,4 @@
 return {
-	ExtraLspAttachMappings = "Modular_ExtraLspAttachMappings",
-	ExtraLspDetachMappings = "Modular_ExtraLspDetachMappings",
 	ExtraHlsCommands = "Modular_ExtraHlsCommands",
 	ExtraXdefaultsFiles = "Modular_ExtraXdefaultsFiles",
 }
--- lua/modular/mappings/Lsp/nvim-lspconfig.lua
+++ lua/modular/mappings/Lsp/nvim-lspconfig.lua
@@ -25,29 +25,24 @@ vim.api.nvim_create_autocmd ("LspAttach", {
 			map ("n", "<c-s>", vim.lsp.buf.signature_help, { buffer = event.buf })
 		end
 
-		-- map("n", "<leader>la", vim.lsp.buf.code_action, { buffer = event.buf })
-		map("n", "<leader>lgD", vim.lsp.buf.declaration, { buffer = event.buf })
-		map("n", "<leader>lgd", vim.lsp.buf.definition, { buffer = event.buf })
-		-- map("n", "<leader>lgi", vim.lsp.buf.implementation, { buffer = event.buf })
-		-- map("n", "<leader>lgr", vim.lsp.buf.references, { buffer = event.buf })
-		map("n", "<leader>lgt", vim.lsp.buf.type_definition, { buffer = event.buf })
-		-- map("n", "<leader>lr", vim.lsp.buf.rename, { buffer = event.buf })
-		map("n", "<leader>ls", vim.cmd.ClangdSwitchSourceHeader, { buffer = event.buf })
-		-- map("n", "<leader>lS", vim.lsp.buf.signature_help, { buffer = event.buf })
-		map("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, { buffer = event.buf })
-		map("n", "<leader>lwl", function()
+		map("n", "grD", vim.lsp.buf.definition, { buffer = event.buf })
+		map("n", "grd", vim.lsp.buf.declaration, { buffer = event.buf })
+		map("n", "grt", vim.lsp.buf.type_definition, { buffer = event.buf })
+		map("n", "grs", vim.cmd.ClangdSwitchSourceHeader, { buffer = event.buf })
+		map("n", "grwa", vim.lsp.buf.add_workspace_folder, { buffer = event.buf })
+		map("n", "grwl", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, { buffer = event.buf })
-		map("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, { buffer = event.buf })
+		map("n", "grwr", vim.lsp.buf.remove_workspace_folder, { buffer = event.buf })
 
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 
 		if client and utils.client_supports_method (client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-			map("n", "<leader>lh", function()
+			map("n", "grh", function()
 				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
 			end, { buffer = event.buf })
 
-			map("n", "<leader>lH", function()
+			map("n", "grH", function()
 				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 			end, { buffer = event.buf })
 		end
@@ -66,24 +61,19 @@ vim.api.nvim_create_autocmd("LspDetach", {
 			unmap ("n", "<c-s>", { buffer = event.buf })
 		end
 
-		-- unmap("n", "<leader>la", { buffer = event.buf })
-		unmap("n", "<leader>lgD", { buffer = event.buf })
-		unmap("n", "<leader>lgd", { buffer = event.buf })
-		-- unmap("n", "<leader>lgi", { buffer = event.buf })
-		-- unmap("n", "<leader>lgr", { buffer = event.buf })
-		unmap("n", "<leader>lgt", { buffer = event.buf })
-		-- unmap("n", "<leader>lr", { buffer = event.buf })
-		unmap("n", "<leader>ls", { buffer = event.buf })
-		-- unmap("n", "<leader>lS", { buffer = event.buf })
-		unmap("n", "<leader>lwa", { buffer = event.buf })
-		unmap("n", "<leader>lwl", { buffer = event.buf })
-		unmap("n", "<leader>lwr", { buffer = event.buf })
+		unmap("n", "grD", { buffer = event.buf })
+		unmap("n", "grd", { buffer = event.buf })
+		unmap("n", "grt", { buffer = event.buf })
+		unmap("n", "grs", { buffer = event.buf })
+		unmap("n", "grwa", { buffer = event.buf })
+		unmap("n", "grwl", { buffer = event.buf })
+		unmap("n", "grwr", { buffer = event.buf })
 
 		local client = vim.lsp.get_client_by_id (event.data.client_id)
 
 		if client and utils.client_supports_method (client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-			unmap("n", "<leader>lh", { buffer = event.buf })
-			unmap("n", "<leader>lH", { buffer = event.buf })
+			unmap("n", "grh", { buffer = event.buf })
+			unmap("n", "glH", { buffer = event.buf })
 		end
 	end,
 })
