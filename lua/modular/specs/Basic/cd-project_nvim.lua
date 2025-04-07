065892c04104bd7c74b565b35ece67e398cabe6c
GasparVardanyan <gaspar_pm@proton.me>
2025-04-07 05:21:09 +0400

    fix

--- lua/modular/specs/Basic/cd-project_nvim.lua
+++ lua/modular/specs/Basic/cd-project_nvim.lua
@@ -33,6 +33,7 @@ return {
 							endif
 							if filereadable("CMakeLists.txt")
 								CMakeSelectCwd .
+								CMakeSelectBuildDir out
 							endif
 							if filereadable(".exrc")
 								source .exrc
