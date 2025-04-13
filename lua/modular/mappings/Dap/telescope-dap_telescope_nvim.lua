4dbb9482f9422a81d18e01b90db9cba1783920eb
GasparVardanyan <gaspar_pm@proton.me>
2025-04-14 00:12:13 +0400

    backup

--- lua/modular/mappings/Dap/telescope-dap_telescope_nvim.lua
+++ lua/modular/mappings/Dap/telescope-dap_telescope_nvim.lua
@@ -1,7 +1,8 @@
 local map = vim.keymap.set
+local telescope = require ("telescope")
 
-map ("n", "<leader>dfc", require ("telescope").extensions.dap.commands, { desc = "commands" })
-map ("n", "<leader>dfC", require ("telescope").extensions.dap.configurations, { desc = "configurations" })
-map ("n", "<leader>dfb", require ("telescope").extensions.dap.list_breakpoints, { desc = "list breakpoints" })
-map ("n", "<leader>dfv", require ("telescope").extensions.dap.variables, { desc = "variables" })
-map ("n", "<leader>dff", require ("telescope").extensions.dap.frames, { desc = "frames" })
+map ("n", "<leader>dfc", telescope.extensions.dap.commands, { desc = "commands" })
+map ("n", "<leader>dfC", telescope.extensions.dap.configurations, { desc = "configurations" })
+map ("n", "<leader>dfb", telescope.extensions.dap.list_breakpoints, { desc = "list breakpoints" })
+map ("n", "<leader>dfv", telescope.extensions.dap.variables, { desc = "variables" })
+map ("n", "<leader>dff", telescope.extensions.dap.frames, { desc = "frames" })
