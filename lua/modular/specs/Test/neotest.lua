83f51943430d36fa60fcac6e17eadf9323cbdedf
GasparVardanyan <gaspar_pm@proton.me>
2025-05-23 23:17:16 +0400

    backup

--- lua/modular/specs/Test/neotest.lua
+++ lua/modular/specs/Test/neotest.lua
@@ -4,13 +4,15 @@ return {
 		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
 		"alfaix/neotest-gtest",
+		"nvim-neotest/neotest-python",
 		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter"
+		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function ()
 		require("neotest").setup({
 			adapters = {
-				require("neotest-gtest").setup({})
+				require ("neotest-gtest").setup ({}),
+				require ("neotest-python")
 			}
 		})
 	end,
