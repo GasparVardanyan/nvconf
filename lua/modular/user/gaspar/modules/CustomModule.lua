9e6567250a65037c209dae923d349cf0cffe2e94
GasparVardanyan <gaspar_pm@proton.me>
2025-06-01 18:09:53 +0400

    backup

--- lua/modular/user/gaspar/modules/CustomModule.lua
+++ lua/modular/user/gaspar/modules/CustomModule.lua
@@ -7,7 +7,10 @@ local CustomModule = Module:new ({
 		require ("modular.user.gaspar.specs.Custom.perfanno_nvim"),
 		-- require ("modular.user.gaspar.specs.Custom.lualine_nvim"),
 		require ("modular.user.gaspar.specs.Custom.csvview_nvim"),
-		require ("modular.user.gaspar.specs.Custom.treesj")
+		require ("modular.user.gaspar.specs.Custom.treesj"),
+		require ("modular.user.gaspar.specs.Custom.nvim-fundo"),
+		require ("modular.user.gaspar.specs.Custom.nvim-bqf"),
+		require ("modular.user.gaspar.specs.Custom.fzf"),
 	},
 })
 
---
+++ lua/modular/user/gaspar/specs/Custom/nvim-bqf.lua
@@ -0,0 +1,8 @@
+return {
+	"kevinhwang91/nvim-bqf",
+	dependencies = {
+		"junegunn/fzf",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	filetype = "qf",
+}
