c2d643f255d940975b957986e1bb48602f304abf
GasparVardanyan <gaspar_pm@proton.me>
2025-07-26 03:26:02 +0400

    backup

--- lua/modular/specs/Compiler/cmake-tools_nvim.lua
+++ lua/modular/specs/Compiler/cmake-tools_nvim.lua
@@ -1,14 +1,26 @@
-local nproc = tonumber (vim.fn.system ({"nproc"}))
-local cmake_build_options = {}
-
-if 0 ~= nproc
-then
-	vim.list_extend (cmake_build_options, { "-j" .. nproc })
-end
-
 return {
 	"Civitasv/cmake-tools.nvim",
 	config = function ()
+		local nproc = tonumber (vim.fn.system ({"nproc"}))
+		local cmake_build_options = {}
+
+		if 0 ~= nproc
+		then
+			vim.list_extend (cmake_build_options, { "-j" .. (nproc - 1) })
+		end
+
+		if vim.fn.filereadable ("/usr/bin/clang") and vim.fn.filereadable ("/usr/bin/clang++")
+		then
+			vim.fn.setenv ("CC", "/usr/bin/clang")
+			vim.fn.setenv ("CXX", "/usr/bin/clang++")
+		end
+
+		if vim.fn.filereadable ("/usr/bin/ccache")
+		then
+			vim.fn.setenv ("CMAKE_C_COMPILER_LAUNCHER", "ccache")
+			vim.fn.setenv ("CMAKE_CXX_COMPILER_LAUNCHER", "ccache")
+		end
+
 		require ("cmake-tools").setup {
 			cmake_build_options = cmake_build_options
 		}
