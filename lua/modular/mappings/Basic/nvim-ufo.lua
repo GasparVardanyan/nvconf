eafd55a9af96192aae2fd190d8e2752dcf1a31a9
GasparVardanyan <gaspar_pm@proton.me>
2026-02-09 23:41:15 +0400

    backup

--- lua/modular/mappings/Basic/nvim-ufo.lua
+++ lua/modular/mappings/Basic/nvim-ufo.lua
@@ -1,4 +1,11 @@
 local map = vim.keymap.set
 
-map ("n", "zR", require("ufo").openAllFolds)
-map ("n", "zM", require("ufo").closeAllFolds)
+map ("n", "zR", require ("ufo").openAllFolds)
+map ("n", "zM", require ("ufo").closeAllFolds)
+
+map ("n", "K", function ()
+	local winid = require ("ufo").peekFoldedLinesUnderCursor ()
+	if not winid then
+		vim.lsp.buf.hover ()
+	end
+end)
