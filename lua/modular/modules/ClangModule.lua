dd10ee6259c1cff31cad3f85bc0c6630ae94c66f
GasparVardanyan <gaspar_pm@proton.me>
2026-05-17 17:57:52 +0400

    backup

--- lua/modular/modules/ClangModule.lua
+++ lua/modular/modules/ClangModule.lua
@@ -9,7 +9,17 @@ local ClangModule = Module:new ({
 		require ("modular.specs.Clang.insights_nvim"),
 		require ("modular.specs.Clang.compiler-explorer_nvim"),
 		require ("modular.specs.Clang.classlayout_nvim"),
-		-- require ("modular.specs.Clang.nvim-treesitter-cpp-tools"),
+		require ("modular.specs.Clang.nvim-treesitter-cpp-tools"),
+		-- {
+		-- 	"simonwinther/cppman.nvim",
+		-- 	version = "*",
+		-- 	cmd = "CPPMan",
+		-- 	name = "cppfzf",
+		-- 	dependencies = {
+		-- 		"folke/snacks.nvim",
+		-- 	},
+		-- 	opts = {},
+		-- },
 	},
 	actions = {
 		ModuleAction:new ({
@@ -43,7 +53,7 @@ local ClangModule = Module:new ({
 						"--background-index",
 						jnproc,
 						"--header-insertion=iwyu",
-						"--clang-tidy",
+						-- "--clang-tidy",
 					},
 				}
 
@@ -54,13 +64,17 @@ local ClangModule = Module:new ({
 				})
 
 				LintersByFt ["c"] = {
-					-- "clangtidy",
+					"clangtidy",
 					"cppcheck",
+					"clazy",
+					-- "cpplint",
 				}
 
 				LintersByFt ["cpp"] = {
-					-- "clangtidy",
+					"clangtidy",
 					"cppcheck",
+					"clazy",
+					-- "cpplint",
 				}
 
 				Formatters.formatters_by_ft ["c"] = {
@@ -70,6 +84,54 @@ local ClangModule = Module:new ({
 				Formatters.formatters_by_ft ["cpp"] = {
 					"clang-format",
 				}
+			end
+		}),
+		ModuleAction:new ({
+			plugins = "nvim-lint",
+			action = function ()
+				local clang_standard = function ()
+					if vim.bo.filetype == "cpp" then
+						return "--extra-arg=-std=c++20"
+					elseif vim.bo.filetype == "c" then
+						return "--extra-arg=-std=c18"
+					else
+						return ""
+					end
+				end
+
+				local cppcheck_standard = function ()
+					if vim.bo.filetype == "cpp" then
+						return "--std=c++20"
+					elseif vim.bo.filetype == "c" then
+						return "--std=c18"
+					else
+						return ""
+					end
+				end
+
+				local nproc = require ("modular.utils").nproc
+				local cppcheck_jnproc = ''
+
+				if 0 ~= nproc
+				then
+					cppcheck_jnproc =  "-j " .. (nproc - 1)
+				end
+
+				local clazy = require 'lint.linters.clazy'
+				vim.list_extend (clazy.args, {
+					clang_standard,
+					"-checks=level0,level1,level2,assert-with-side-effects,compare-member-check,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,isempty-vs-count,jni-signatures,qbytearray-conversion-to-c-style,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-ref,qstring-varargs,qt-keyword-emit,qt-keywords,qvariant-template-instantiation,raw-environment-function,reserve-candidates,sanitize-inline-keyword,signal-with-return-value,thread-with-slots,tr-non-literal,unexpected-flag-enumerator-value,unneeded-cast,unused-result-check,use-arrow-operator-instead-of-data,use-chrono-in-qtimer,used-qunused-variable"
+				})
+				local clang_tidy = require ("lint.linters.clangtidy")
+				vim.list_extend (clang_tidy.args, {
+					clang_standard,
+					"--checks='clang-diagnostic-*,bugprone-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,portability-*,readability-*,-fuchsia-*,-google-*,-llvm-*,-readability-magic-numbers'"
+				})
+				local cppcheck = require ("lint.linters.cppcheck")
+				vim.list_extend (cppcheck.args, {
+					cppcheck_standard,
+					cppcheck_jnproc
+				})
 			end
 		}),
 		ModuleAction:new ({
