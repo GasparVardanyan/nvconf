bfab3851fd3e48e9f35121b59fb9e0242ace1053
GasparVardanyan <gaspar_pm@proton.me>
2026-06-29 16:35:26 +0400

    backup

--- lua/modular/modules/LspModule.lua
+++ lua/modular/modules/LspModule.lua
@@ -17,6 +17,7 @@ local LspModule = Module:new ({
 		require ("modular.specs.Lsp.goto-preview"),
 		require ("modular.specs.Lsp.dropbar_nvim"),
 		require ("modular.specs.Lsp.nvim-lsp-file-operations"),
+		require ("modular.specs.Lsp.lspkind_nvim"),
 		-- require ("modular.specs.Lsp.blink_cmp"),
 		-- require ("modular.specs.Lsp.lspsaga_nvim"),
 	},
